2d2bad76cf92e19b75f3b615de317c96a8db3319
sigmadev92 <devanshofficial2000@gmail.com>
2025-11-02 23:27:10 +0530

    setup react-router

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <header>
+      <div>
+        <h1>BlogsEra</h1>
+      </div>
+      <nav></nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <section>
+      <h2>Dashboard</h2>
+    </section>
+  );
+};
+
+export default Dashboard;
