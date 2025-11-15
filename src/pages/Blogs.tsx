54763ab6eb789ddd41ddf07ba4d353c1c3140d52
sigmadev92 <devanshofficial2000@gmail.com>
2025-11-15 22:35:48 +0530

    created blog listing page

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,5 +1,56 @@
+import { useEffect, useState } from "react";
+import { blogsURL } from "../functions/backend";
+import BlogBox from "../components/ui/BlogBox";
+import { type PublicBlog1 } from "../types/blog";
+
 const Blogs = () => {
-  return <div>Blogs</div>;
+  const [blogs, setBlogs] = useState<PublicBlog1[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetch(`${blogsURL}/all`);
+        const data = await response.json();
+
+        console.log(data.blogs);
+        setBlogs(data.blogs);
+      } catch (error) {
+        console.log(error);
+        setError("Client Error. Working on it");
+      }
+    };
+    fetchBlogs();
+  }, []);
+  return (
+    <section className="theme pt-11 h-full">
+      <div className="flex gap-2 px-4 h-[95%]">
+        <div className="md:w-[15%] hidden md:block border-light md:h-full">
+          <h3 className="font-bold text-[14px] text-center">Categories</h3>
+        </div>
+        <div className="md:w-[83%] border-light h-full">
+          {blogs.length > 0 ? (
+            <div className="w-full h-full flex justify-center overflow-scroll p-1">
+              <ul className="flex gap-2 flex-wrap list-none">
+                {blogs.map((blogItem, idx) => (
+                  <BlogBox
+                    key={idx}
+                    blog={blogItem}
+                    author={{
+                      ...blogItem.authorId,
+                      authorId: blogItem.authorId._id,
+                    }}
+                  />
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <div className="center">{error}</div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blogs;
